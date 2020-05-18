ac15e416a6124a022997b7a539dfecf97cfcacc6
kimhaneui <kimhaneui2@naver.com>
2020-05-18 11:47:47 +0900

    기본메뉴 출력

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -1,5 +1,29 @@
 package code;
 
+import java.util.Scanner;
+
 public class MainDrive {
+	
+	public static void main(String[] args) {
+		printMenu();
+	}
+	
+	static void printMenu() {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("***전화번호부***");
+		System.out.println("1. 전화번호 추가");
+		System.out.println("2. 전체 목록 조회");
+		System.out.println("0. 프로그램 종료");
+		System.out.println("***************");
+		
+		while(true) {
+			System.out.println("메뉴입력 : ");
+			
+			int menuInput = scan.nextInt();
+		}
+		
+	}
 
 }
